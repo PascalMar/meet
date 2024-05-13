972d028aaae7af6dd1c16f94aa59d0b8fb5daa00
Pascal Marienfeld <pa.marienfeld@gmail.com>
2024-05-13 17:20:43 +0200

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './bootstrap.min.css'
-import './bootstrap.min.css'
+
 
 import './App.css';
 
